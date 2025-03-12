da4e404655d58a99d8345377797683bf47a12ab1
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-03-12 14:52:27 +0100

    Created viewmodel for customer and a service class for customers page

---
+++ BankWebApp/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BankWebApp.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public int CustomerId { get; set; }
+        public string GivenName { get; set; } = null!;
+        public string SurName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Country { get; set; } = null!;
+    }
+}
---
+++ Services/CustomerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
+using Services.Intefaces;
+
+namespace Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly BankAppDataContext _dbContext;
+
+        public CustomerService(BankAppDataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
