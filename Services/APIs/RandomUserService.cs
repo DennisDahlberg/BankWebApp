fbe3038a110185d413dbf90b12bd85a5b0aee8a3
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-03-15 19:23:27 +0100

    started working on API call to RandomUser to get random image for customer card

---
+++ Services/APIs/RandomUserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.APIs
+{
+    public class RandomUserService
+    {
+        public readonly string _baseUrl = "https://randomuser.me/api/?gender=";
+        public readonly HttpClient _httpClient;
+
+        public RandomUserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> FetchFromApi(string gender)
+        {
+            if (string.IsNullOrEmpty(gender))
+                gender = "female";
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}{gender}");
+
+            var response = await _httpClient.SendAsync(request);
+
+            return null;
+        }
+    }
+}
