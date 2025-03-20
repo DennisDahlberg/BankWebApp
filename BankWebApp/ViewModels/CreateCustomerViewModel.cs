38c95bb63795c2b1c44bebba656792c06898b09a
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-03-20 16:05:00 +0100

    Started working on createcustomer viewmodel

---
+++ BankWebApp/ViewModels/CreateCustomerViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankWebApp.ViewModels
+{
+    public class CreateCustomerViewModel
+    {
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string Givenname { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string Streetaddress { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country {  get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string Zipcode { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Emailaddress { get; set; }
+
+        [Required]
+        public string Phonenumber { get; set; }
+    }
+}
