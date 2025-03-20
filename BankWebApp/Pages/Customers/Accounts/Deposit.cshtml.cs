1fd822d4d41fce33a11d93d62077d3f2a29fb254
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-03-20 13:09:24 +0100

    Updated styling in deposit and started working on deposit form

--- BankWebApp/Pages/Customers/Accounts/Deposit.cshtml.cs
+++ BankWebApp/Pages/Customers/Accounts/Deposit.cshtml.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Services.Intefaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace BankWebApp.Pages.Customers.Accounts
 {
+    [BindProperties]
     public class DepositModel : PageModel
     {
         private readonly IAccountService _accountService;
@@ -16,11 +18,24 @@ namespace BankWebApp.Pages.Customers.Accounts
         public int AccountId { get; set; }
         public int CustomerId { get; set; }
         public decimal Balance { get; set; }
+
+        [Range(100,1000)]
+        public decimal Deposit { get; set; }
+
         public void OnGet(int accountId, int customerId)
         {
             AccountId = accountId;
             CustomerId = customerId;
             Balance = _accountService.GetAccount(accountId).Balance;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return Page();
         }
     }
 }
