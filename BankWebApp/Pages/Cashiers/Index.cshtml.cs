9b326218c50aa4532d8b6795e2ecef7e5ab67726
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-03-27 13:03:12 +0100

    Added a folder called cashiers and made the index razor page for it

---
+++ BankWebApp/Pages/Cashiers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankWebApp.Pages.Cashiers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
