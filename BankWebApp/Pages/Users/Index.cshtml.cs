44469c8195e1754e831fe991f0982ab37ae2450a
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-03-27 13:51:53 +0100

    Made the user data accessible in the backend

--- BankWebApp/Pages/Users/Index.cshtml.cs
+++ BankWebApp/Pages/Users/Index.cshtml.cs
@@ -1,14 +1,32 @@
+using BankWebApp.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Intefaces;
 
 namespace BankWebApp.Pages.Users
 {
     [Authorize(Roles = "Admin")]
     public class IndexModel : PageModel
     {
+        private readonly IUserService _userService;
 
-        public void OnGet()
+        public IndexModel(IUserService userService)
         {
+            _userService = userService;
+        }
+
+        public List<UserViewModel> Users { get; set; }
+
+        public async Task OnGet()
+        {
+            var userDTOs = await _userService.GetAllUsersAsync();
+
+            Users = userDTOs.Select(u => new UserViewModel()
+            {
+                UserId = u.UserId,
+                Email = u.Email,
+                Role = u.Role,
+            }).ToList();
         }
     }
 }
