1719b2597333ac7bcc7aa69ddd97934c9b7e04cf
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-04-15 14:40:58 +0200

    Created a top earners razor page and added the basic html to make it work with the layout

---
+++ BankWebApp/Pages/TopEarners.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankWebApp.Pages
+{
+    public class TopEarnersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
